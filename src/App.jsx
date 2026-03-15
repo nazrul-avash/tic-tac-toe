9aa16c2255fb838fba293a9ed8185f8680399c8d
nazrul-avash <nazrul.avash@gmail.com>
2026-03-15 20:42:58 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+export function Square({value, onSquareClick}){
+  
+        return(
+          <div>
+            <button className='square' onClick={onSquareClick}>{value}</button>
+            </div>
+        );
+  }
+
+
+
+function Board({squares}){
+  function handleClick(i){
+    if(squares[i]){
+      return;
+    }
+    const nextSetOfSquares = squares.slice();
+  }
+  
+  return(
+    <div className="board">
+    <div >
+      <Square  value={squares[0]} onSquareClick={() =>handleClick(0)} />
+      <Square  value={squares[1] } onSquareClick={() =>handleClick(1)} />
+      <Square  value={squares[2]} onSquareClick={() =>handleClick(2)} />
+      
+    </div>
+    <div >
+      <Square  value={squares[3]} onSquareClick={() =>handleClick(3)} />
+      <Square  value={squares[4]} onSquareClick={() =>handleClick(4)}/>
+      <Square  value={squares[5]} onSquareClick={() =>handleClick(5)}/>
+      
+    </div>
+    <div >
+      <Square  value={squares[6]} onSquareClick={() =>handleClick(6)}/>
+      <Square  value={squares[7]} onSquareClick={() =>handleClick(7)}/>
+      <Square  value={squares[8]} onSquareClick={() =>handleClick(8)}/>
+      
+    </div>
+   
+  
+  
+    </div>
+  );
+}
+function Game(){
+  
+  
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext =  
+ 
+  return(
+    <Board squares={squares} />
+  );
+}
+
+function App() {
+
+  return (
+    <Game/>
+  )
+}
+
+export default App
